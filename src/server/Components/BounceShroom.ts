f7d86d2c55c66db8ac238b93c35718d5a63d95f1
Spencer Elkington <spelkington@gmail.com>
2021-10-04 18:20:16 -0700

    BounceShroom can launch correctly (sometimes)

--- src/server/Components/BounceShroom.ts
+++ src/server/Components/BounceShroom.ts
@@ -3,8 +3,10 @@ import { Workspace } from "@rbxts/services";
 import { Vector } from "ts-matrix";
 
 type BounceShroomModel = Model & {
-	Anchor: Part;
-	Destination: Part;
+	Markers: Model & {
+		Anchor: Part;
+		Destination: Part;
+	};
 	Configuration: Configuration & {
 		Debug: BoolValue;
 		Height: IntValue;
@@ -20,11 +22,20 @@ type BounceShroomModel = Model & {
 
 class BounceShroom implements Component.ComponentClass {
 	public static Tag = "BounceShroom";
+	private static DIVE_TILT = -math.pi / 4;
+	private static USERCONTROL_VELOCITY = 0;
+	private static LAUNCH_TIME = 0.1;
+	private static FALLCHECK_TIME = 0.5;
+	private static DEBOUNCE_TIME = 1;
+	private static SPIN_DAMPEN = math.pi / 6;
+	private static SPIN_MAX = math.pi * 8;
+	private static VELOCITY_ADJ = 0.75;
 
 	private janitor = new Janitor();
 	private debounce: Set<Model> = new Set<Model>();
 	private model: BounceShroomModel;
 	private mushroom;
+	private markers;
 	private angle = 0;
 	private velocity = 0;
 
@@ -37,8 +48,11 @@ class BounceShroom implements Component.ComponentClass {
 		assert(instance.IsA("Model"));
 		this.model = <BounceShroomModel>instance;
 		this.mushroom = this.model.MushroomModel;
+		this.markers = this.model.Markers;
 
 		this.setPhysics();
+
+		this.janitor.Add(this.model);
 
 		this.janitor.Add(
 			this.mushroom.Launcher.Touched.Connect((part) => {
@@ -52,8 +66,8 @@ class BounceShroom implements Component.ComponentClass {
 	}
 
 	private setPhysics() {
-		const destination = this.model.Destination;
-		const anchor = this.model.Anchor;
+		const destination = this.markers.Destination;
+		const anchor = this.markers.Anchor;
 		const launcher = this.mushroom.Launcher;
 		const debug = this.model.Configuration.Debug.Value;
 
@@ -75,35 +89,40 @@ class BounceShroom implements Component.ComponentClass {
 		// Calculate vertex coordinates
 		const x_vert = -b / (2 * a);
 		const y_vert = c - (b * b) / (4 * a);
-		if (debug) {
-			anchor.Position = new Vector3(x_vert, y_vert, 0);
-		}
 
 		// Finalize physics predictions
 		const gravity = -1 * Workspace.Gravity;
 		const y_dist = y_vert - y1;
-		const x_dist = math.abs(x_vert - x1) * 2;
+		const x_dist = x_vert - x1 * 2;
 
 		const timeInAir = math.sqrt((-2 * y_dist) / gravity);
-		const x_vel = x_dist / timeInAir;
+		const x_vel = math.abs(x_dist) / timeInAir;
 		const y_vel = -1 * gravity * timeInAir;
-		this.velocity = math.sqrt(x_vel * x_vel + y_vel * y_vel);
-		this.angle = math.tan(x_vel / y_vel);
+		this.velocity = math.sqrt(x_vel * x_vel + y_vel * y_vel) * BounceShroom.VELOCITY_ADJ;
+		this.angle = math.sign(x_dist) * math.tan(x_vel / y_vel) + math.pi / 16;
 
 		// Cleanup
 		if (!debug) {
 			destination.Destroy();
 			anchor.Destroy();
 		} else {
-			destination.Parent = Workspace;
-			anchor.Parent = Workspace;
-			this.janitor.Add(destination);
-			this.janitor.Add(anchor);
+			const x_step = math.sign(x_dist) * 5;
+			for (let x_marker = x1; x_marker < x3; x_marker += x_step) {
+				// Calculate marker X and Y
+				const y_marker = a * x_marker * x_marker + b * x_marker + c;
+				const z_marker = anchor.Position.Z;
+
+				// Create marker
+				const marker = anchor.Clone();
+				marker.Parent = anchor.Parent;
+				marker.Position = new Vector3(x_marker, y_marker, z_marker);
+			}
 		}
 
 		const newCFrame = this.model.GetPivot();
 
-		this.model.PivotTo(newCFrame.mul(CFrame.fromOrientation(0, 0, this.angle - math.pi / 2)));
+		print(this.angle);
+		this.mushroom.PivotTo(newCFrame.mul(CFrame.fromOrientation(0, 0, this.angle - math.pi / 2)));
 	}
 
 	private onTouched(part: BasePart) {
@@ -118,8 +137,50 @@ class BounceShroom implements Component.ComponentClass {
 	}
 
 	private launchActor(actor: Model) {
-		print(actor);
-		wait(1);
+		// Humanoid is verified pre-call, so we can assume it has one.
+		const humanoid: Humanoid = <Humanoid>actor.FindFirstChild("Humanoid")!;
+		// Try to find the primary part - fall back to HRP or first basepart
+		const rootPart: BasePart = <BasePart>(
+			(actor.PrimaryPart ?? actor.FindFirstChild("HumanoidRootPart") ?? actor.FindFirstChildOfClass("BasePart")!)
+		);
+		const upVector = this.mushroom.Launcher.CFrame.UpVector;
+
+		// Choose the dive direction based on the tilt of the launch
+		let diveDirection = 0;
+		if (upVector.X > 0) {
+			diveDirection = math.pi + BounceShroom.DIVE_TILT * 2;
+		}
+
+		// Immobilize the player and warp them to the top of the launcher
+		humanoid.PlatformStand = true;
+		rootPart.CFrame = this.mushroom.Launcher.CFrame.mul(
+			CFrame.Angles(0, math.pi / 2 - BounceShroom.DIVE_TILT + diveDirection, 0),
+		).add(upVector.mul(3));
+
+		// Create new force to launch the player
+		const launchForce = new Instance("BodyVelocity");
+		launchForce.Parent = rootPart;
+		launchForce.MaxForce = new Vector3(math.huge, math.huge, math.huge);
+		launchForce.Velocity = upVector.mul(this.velocity);
+
+		// Create new force to spin the player during launch
+		const spinForce = new Instance("BodyAngularVelocity");
+		spinForce.Parent = rootPart;
+		spinForce.MaxTorque = new Vector3(math.huge, math.huge, math.huge);
+		spinForce.AngularVelocity = upVector.mul(
+			math.min(BounceShroom.SPIN_MAX, this.velocity / BounceShroom.SPIN_DAMPEN),
+		);
+
+		wait(BounceShroom.LAUNCH_TIME);
+		launchForce.Destroy();
+		spinForce.Destroy();
+
+		wait(BounceShroom.FALLCHECK_TIME);
+		while (rootPart.AssemblyLinearVelocity.Y > BounceShroom.USERCONTROL_VELOCITY) {
+			wait(0.1);
+		}
+
+		humanoid.PlatformStand = false;
 	}
 
 	/**
